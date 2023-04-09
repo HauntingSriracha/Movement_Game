2d9d3930d9e08b2dad36a251d6ad6b62e443717d
HauntingSriracha <{HauntingSriracha}@users.noreply.github.com>
2023-04-09 12:30:50 -0400

    Commit everything!

---
+++ Tiger Games/Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float SpeedOg;
+    public float SpeedCurrent;
+    public float BackSpeed;
+    public float UpSpeed;
+    public float DownSpeed;
+    public float HorizontalInput;
+    public float VerticalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+        SpeedCurrent = SpeedOg;
+
+        if (HorizontalInput < 0.00f)
+            SpeedCurrent = BackSpeed;
+
+        if (VerticalInput > 0.00f & HorizontalInput > 0.00f)
+            SpeedCurrent = UpSpeed;
+        
+        if (VerticalInput < 0.00f)
+            SpeedCurrent = DownSpeed;
+       
+      
+
+
+        transform.Translate(Vector3.right * Time.deltaTime * HorizontalInput * SpeedCurrent);
+
+    }
+}
